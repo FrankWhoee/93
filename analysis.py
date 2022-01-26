b894c5b22e4c39b8a7d8fca45fb098f06a245281
Frank Hui <huifrank@live.com>
2022-01-25 17:43:07 -0800

    Add analysis

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import numpy as np
 
 if (not os.path.exists("data.pickle")):
     print("No data found. Running main.py...")
@@ -15,22 +16,37 @@ total = 0
 normal84 = 0
 normal102 = 0
 
+summary = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+sumtotal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
 for game in data:
     ctwins = 0
     twins = 0
     cursed = 0
+
+    majority_half_end_score = 0
+    half_favours = 0
     for r in game:
-        if ctwins == 9 and twins == 3:
-            cursed = 1
-        elif ctwins == 3 and twins == 9:
-            cursed = -1
-        if r < 0:
-            twins += 1
-        elif r > 0:
+        if ctwins + twins == 12:
+            majority_half_end_score = ctwins if ctwins > twins else twins
+            if ctwins > twins:
+                half_favours = 1
+            else:
+                half_favours = -1
+        if r > 0:
             ctwins += 1
-    if (ctwins < twins and cursed == 1) or (ctwins > twins and cursed == -1):
-        success += 1
+        elif r < 0:
+            twins += 1
+    summary[majority_half_end_score] += 1 if (half_favours == -1 and ctwins > twins) or (
+                half_favours == 0 and twins > ctwins) else 0
+    sumtotal[majority_half_end_score] += 1
     total += 1
-print(success)
-print(total)
-print(success/total)
+
+print("Games analyzed: " + str(total))
+winrate = np.asarray(summary[6:13]) / np.asarray(sumtotal[6:13])
+print("Chance of losing from 7-5: " + str(winrate[1]))
+print("Chance of losing from 8-4: " + str(winrate[2]))
+print("Chance of losing from 9-3: " + str(winrate[3]))
+print("Chance of losing from 10-2: " + str(winrate[4]))
+print("Chance of losing from 11-1: " + str(winrate[5]))
+print("Chance of losing from 12-0: " + str(winrate[6]))
